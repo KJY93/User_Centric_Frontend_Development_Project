932670fd7d1ae846ceacffdad80c6d741f6828e1
Ubuntu <ubuntu@ip-172-31-40-151.us-east-2.compute.internal>
2019-09-26 16:20:15 +0000

    added index.js which will display the current active page to the user

--- static/index.js
+++ static/index.js
@@ -1,15 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => { 
+// added interactivity to show user which current page they are currently on using javascript  
+document.addEventListener('DOMContentLoaded', () => {
     
-    var navlinkitem = document.querySelectorAll('nav a');
+    // get all the <a> tag elements that resides within the <li> tag which can be found inside the <nav> bar
+    var navlinkitem = document.querySelectorAll('nav li a');
     
-    // Loop through the <a> tag and add the active class to the current/clicked link
+    // for loop to check the href provided in the <a> tag href with the current url
+    // if there is a match, add the activepage class to the active element
     for (var i = 0; i < navlinkitem.length; i++) {
-        navlinkitem[i].addEventListener("click", function() {
-            var current = document.getElementsByClassName('.activelink');
-            current.classList.remove('activelink');
-            this.classList.add('activelink');
-    
-        })}
+        
+        if (navlinkitem[i].href == window.location.href) {
+            navlinkitem[i].parentElement.classList.add('activepage');
+        }
+    }  
 });
+
+
 
 
